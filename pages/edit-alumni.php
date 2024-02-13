bd4c801a5dcbe354fb79639b4fed4de7947fa795
Nandang Setia Nugraha <48231636+nandangsetianugraha@users.noreply.github.com>
2024-02-13 12:40:27 +0700

    Update edit-alumni.php

--- pages/edit-alumni.php
+++ pages/edit-alumni.php
@@ -477,15 +477,13 @@ $bulan = array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "
                                                                     }else{
                                                                     ?>
                                                                     <div class="alert alert-outline-secondary" id="itung">
-                                                                        <div class="alert-icon">
-                                                                            <i class="fa fa-lightbulb"></i>
-                                                                        </div>
                                                                         <div class="alert-content"> 
                                                                         <button class="btn btn-sm btn-outline-info" data-doc="<?=$pn['peserta_didik_id'];?>" data-bs-toggle="modal" data-bs-target="#info">
 																			<i class="fa-solid fa-magnifying-glass"></i> <?=$regis['file_ijazah'];?>
 																		</button>
                                                                         </div>
                                                                     </div>
+																	<div id="canvassss"></div>
                                                                     <?php 
                                                                     }
                                                                     ?>
@@ -667,6 +665,66 @@ $bulan = array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "
 	
 	var pdfjsLib = window['pdfjs-dist/build/pdf'];
 	pdfjsLib.GlobalWorkerOptions.workerSrc = '<?=base_url();?>assets/js/pdf.worker.min.js';
+	
+	<?php if(empty($regis['file_ijazah']) or $regis['file_ijazah']==''){}else{ ?>
+		var url = '<?=base_url();?>ijazah/sd/<?=$regis['file_ijazah'];?>';
+		var pdfjsLib = window['pdfjs-dist/build/pdf'];
+		pdfjsLib.GlobalWorkerOptions.workerSrc = '<?=base_url();?>assets/js/pdf.worker.min.js';
+		var pdfDoc = null,
+			pageNum = 1,
+			pageRendering = false,
+			pageNumPending = null,
+			canvas = document.getElementById('canvassss'),
+			scale = 1.3;
+
+		function renderPage(num, canvas) {
+		  var ctx = canvas.getContext('2d');
+		  pageRendering = true;
+		  // Using promise to fetch the page
+		  pdfDoc.getPage(num).then(function(page) {
+			//var viewport = page.getViewport({scale: scale});
+			//var viewport = page.getViewport(canvas.width / page.getViewport(1.0).width);
+			var viewport = page.getViewport({scale: scale});
+			canvas.height = viewport.height;
+			canvas.width = viewport.width;
+
+			// Render PDF page into canvas context
+			var renderContext = {
+			  canvasContext: ctx,
+			  viewport: viewport
+			};
+			var renderTask = page.render(renderContext);
+
+			// Wait for rendering to finish
+			renderTask.promise.then(function() {
+			  pageRendering = false;
+			  if (pageNumPending !== null) {
+				// New page rendering is pending
+				renderPage(pageNumPending);
+				pageNumPending = null;
+			  }
+			});
+		  });
+		}
+
+		pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
+		  pdfDoc = pdfDoc_;
+
+		  const pages = parseInt(pdfDoc.numPages);
+
+		  var canvasHtml = '';
+		  for (var i = 0; i < pages; i++) {
+			canvasHtml += '<canvas id="canvas_' + i + '"></canvas>';
+		  }
+
+		  document.getElementById('canvassss').innerHTML = canvasHtml;
+
+		  for (var i = 0; i < pages; i++) {
+			var canvas = document.getElementById('canvas_' + i);
+			renderPage(i+1, canvas);
+		  }
+		});		
+	<?php } ?>
 
 	$("#file").on("change", function(e){
 		$("#pdfViewer").show();
